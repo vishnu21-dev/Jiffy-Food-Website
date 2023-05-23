c58a3c63df69c582d25aee73361d15839551d4cf
zebaamk <zebaamk@gmail.com>
2023-05-23 00:46:21 -0700

    Files are added

--- EurekaServer/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ EurekaServer/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -1,9 +1,11 @@
-package com.niit.bej.eurekaserver;
+package com.example.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
--- EurekaServer/src/test/java/com/example/eurekaserver/EurekaServerApplicationTests.java
+++ EurekaServer/src/test/java/com/example/eurekaserver/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.niit.bej.eurekaserver;
+package com.example.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

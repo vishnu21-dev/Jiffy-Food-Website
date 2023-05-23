5c0671da1caccb5d636d8ad6560fc34f31d8a1b8
zebaamk <zebaamk@gmail.com>
2023-05-23 00:18:34 -0700

    AuthentificationService module is created

---
+++ AuthentificationService/src/test/java/com/niit/bej/authentificationservice/AuthentificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.bej.authentificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthentificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

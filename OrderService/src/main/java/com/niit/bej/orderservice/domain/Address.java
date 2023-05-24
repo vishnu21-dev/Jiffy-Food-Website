fc7ad54b1b9e720718030bef162f6737670c60f4
zebaamk <zebaamk@gmail.com>
2023-05-23 22:45:29 -0700

    Address class is created and attributes are added

---
+++ OrderService/src/main/java/com/niit/bej/orderservice/domain/Address.java
@@ -0,0 +1,10 @@
+package com.niit.bej.orderservice.domain;
+
+public class Address {
+
+    private String houseNo;
+    private String streetName;
+    private String city;
+    private String state;
+
+}
--- OrderService/src/main/java/com/niit/bej/orderservice/domain/User.java
+++ OrderService/src/main/java/com/niit/bej/orderservice/domain/User.java
@@ -1,10 +1,12 @@
 package com.niit.bej.orderservice.domain;
 
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Document
 public class User {
 
+    @MongoId
     private String emailId;
     private String password;
     private String phoneNo;

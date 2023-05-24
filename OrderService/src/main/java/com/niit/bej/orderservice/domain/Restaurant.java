f85287011603c0fd45f8d858d777f9a7d8557a83
zebaamk <zebaamk@gmail.com>
2023-05-23 23:29:06 -0700

    Restaurant.java is created

---
+++ OrderService/src/main/java/com/niit/bej/orderservice/domain/Restaurant.java
@@ -0,0 +1,11 @@
+package com.niit.bej.orderservice.domain;
+
+import java.util.List;
+
+public class Restaurant {
+
+    private String name;
+    private List<Cuisine> cuisines;
+    private Address address;
+
+}

26cefa74ec562ec86acd640c2f3d51462a2f1112
zebaamk <zebaamk@gmail.com>
2023-05-24 00:11:18 -0700

    Constructors are added

--- OrderService/src/main/java/com/niit/bej/orderservice/domain/User.java
+++ OrderService/src/main/java/com/niit/bej/orderservice/domain/User.java
@@ -15,4 +15,16 @@ public class User {
     private Address address;
     private List<Restaurant> restaurants;
     private List<Order> orders;
+
+    public User(String emailId, String password, String phoneNo, Address address, List<Restaurant> restaurants, List<Order> orders) {
+        this.emailId = emailId;
+        this.password = password;
+        this.phoneNo = phoneNo;
+        this.address = address;
+        this.restaurants = restaurants;
+        this.orders = orders;
+    }
+
+    public User() {
+    }
 }

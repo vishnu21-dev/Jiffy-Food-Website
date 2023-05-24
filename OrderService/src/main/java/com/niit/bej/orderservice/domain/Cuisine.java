3272637108b10d40a646fae4349ab0e0bd6a78e0
zebaamk <zebaamk@gmail.com>
2023-05-24 01:30:07 -0700

    Getters & Setters, Equals & hashCode are added

--- OrderService/src/main/java/com/niit/bej/orderservice/domain/Cuisine.java
+++ OrderService/src/main/java/com/niit/bej/orderservice/domain/Cuisine.java
@@ -1,6 +1,7 @@
 package com.niit.bej.orderservice.domain;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Cuisine {
 
@@ -29,5 +30,18 @@ public class Cuisine {
 
     public void setDishes(List<Dish> dishes) {
         this.dishes = dishes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cuisine cuisine = (Cuisine) o;
+        return Objects.equals(name, cuisine.name) && Objects.equals(dishes, cuisine.dishes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, dishes);
     }
 }

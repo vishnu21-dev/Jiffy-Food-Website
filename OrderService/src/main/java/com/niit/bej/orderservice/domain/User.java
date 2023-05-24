d6b0f1c48f4c2260a8f0ab964420adbdd60a3852
zebaamk <zebaamk@gmail.com>
2023-05-23 22:41:15 -0700

    Attributes are added to User

--- OrderService/src/main/java/com/niit/bej/orderservice/domain/User.java
+++ OrderService/src/main/java/com/niit/bej/orderservice/domain/User.java
@@ -1,4 +1,13 @@
 package com.niit.bej.orderservice.domain;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class User {
+
+    private String emailId;
+    private String password;
+    private String phoneNo;
+    private Address address;
+
 }

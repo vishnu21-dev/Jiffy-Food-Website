a8738fbe10ea9eee117f2a9b744014961935ffea
zebaamk <zebaamk@gmail.com>
2023-05-24 06:11:18 -0700

    constructor is added

--- OrderService/src/main/java/com/niit/bej/orderservice/exception/UserNotFoundException.java
+++ OrderService/src/main/java/com/niit/bej/orderservice/exception/UserNotFoundException.java
@@ -5,4 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User does not exist!")
 public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }

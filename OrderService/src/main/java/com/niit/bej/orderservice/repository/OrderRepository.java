a9987b4db9fefe8de72f2934b68127e776ae07a9
zebaamk <zebaamk@gmail.com>
2023-05-24 01:51:26 -0700

    OrderRepository.java is created

---
+++ OrderService/src/main/java/com/niit/bej/orderservice/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.niit.bej.orderservice.repository;
+
+import com.niit.bej.orderservice.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends MongoRepository<String, User> {
+}

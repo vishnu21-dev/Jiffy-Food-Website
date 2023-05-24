eb3bcf14b1f6e8b3b98a408ab59a0b93fd8dbc19
zebaamk <zebaamk@gmail.com>
2023-05-24 01:28:47 -0700

    Constructors are added

--- OrderService/src/main/java/com/niit/bej/orderservice/domain/Cuisine.java
+++ OrderService/src/main/java/com/niit/bej/orderservice/domain/Cuisine.java
@@ -6,4 +6,28 @@ public class Cuisine {
 
     private String name;
     private List<Dish> dishes;
+
+    public Cuisine(String name, List<Dish> dishes) {
+        this.name = name;
+        this.dishes = dishes;
+    }
+
+    public Cuisine() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Dish> getDishes() {
+        return dishes;
+    }
+
+    public void setDishes(List<Dish> dishes) {
+        this.dishes = dishes;
+    }
 }

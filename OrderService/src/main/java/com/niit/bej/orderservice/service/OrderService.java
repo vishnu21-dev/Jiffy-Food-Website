4694207f28e0a5a1c51ade1afdb6cf6e37f0c28a
zebaamk <zebaamk@gmail.com>
2023-05-24 06:18:44 -0700

    addOrder method is created

--- OrderService/src/main/java/com/niit/bej/orderservice/service/OrderService.java
+++ OrderService/src/main/java/com/niit/bej/orderservice/service/OrderService.java
@@ -1,9 +1,14 @@
 package com.niit.bej.orderservice.service;
 
+import com.niit.bej.orderservice.domain.Order;
 import com.niit.bej.orderservice.domain.User;
+import com.niit.bej.orderservice.exception.OrderAlreadyExistsException;
 import com.niit.bej.orderservice.exception.UserAlreadyExistsException;
+import com.niit.bej.orderservice.exception.UserNotFoundException;
 
 public interface OrderService {
 
     User registerUser(User user) throws UserAlreadyExistsException;
+
+    User addOrder(Order order, String userId) throws UserNotFoundException, OrderAlreadyExistsException;
 }

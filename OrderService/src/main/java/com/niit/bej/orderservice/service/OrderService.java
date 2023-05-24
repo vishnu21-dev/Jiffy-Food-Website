539a6f3dc82a5062fd99ce02f4ca96836f92e419
zebaamk <zebaamk@gmail.com>
2023-05-24 05:47:09 -0700

    UserAlreadyExistsException class is created

--- OrderService/src/main/java/com/niit/bej/orderservice/service/OrderService.java
+++ OrderService/src/main/java/com/niit/bej/orderservice/service/OrderService.java
@@ -1,8 +1,9 @@
 package com.niit.bej.orderservice.service;
 
 import com.niit.bej.orderservice.domain.User;
+import com.niit.bej.orderservice.exception.UserAlreadyExistsException;
 
 public interface OrderService {
 
-    User registerUser(User user);
+    User registerUser(User user) throws UserAlreadyExistsException;
 }

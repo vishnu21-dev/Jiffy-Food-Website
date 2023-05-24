433a6c58274798d16717f93a086ae439092a3704
zebaamk <zebaamk@gmail.com>
2023-05-24 06:17:23 -0700

    OrderAlreadyExistsException is created

---
+++ OrderService/src/main/java/com/niit/bej/orderservice/exception/OrderAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.niit.bej.orderservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.CONFLICT, reason = "Order already exists!")
+public class OrderAlreadyExistsException extends Exception {
+}

442a585647427199febf7349dfa89753bcf52ae2
zebaamk <zebaamk@gmail.com>
2023-05-24 06:18:15 -0700

    constructor is added

--- OrderService/src/main/java/com/niit/bej/orderservice/exception/OrderAlreadyExistsException.java
+++ OrderService/src/main/java/com/niit/bej/orderservice/exception/OrderAlreadyExistsException.java
@@ -5,4 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.CONFLICT, reason = "Order already exists!")
 public class OrderAlreadyExistsException extends Exception {
+    public OrderAlreadyExistsException(String message) {
+        super(message);
+    }
 }

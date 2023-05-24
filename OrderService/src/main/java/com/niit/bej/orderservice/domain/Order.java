9f0ce23e8b655875c91877442d82d5321935ee7e
zebaamk <zebaamk@gmail.com>
2023-05-24 01:34:54 -0700

    Constructors are added

--- OrderService/src/main/java/com/niit/bej/orderservice/domain/Order.java
+++ OrderService/src/main/java/com/niit/bej/orderservice/domain/Order.java
@@ -5,5 +5,13 @@ import java.util.List;
 public class Order {
 
     private int orderId;
-    private List<Dish> dishes;
+    private List<Restaurant> restaurants;
+
+    public Order(int orderId, List<Restaurant> restaurants) {
+        this.orderId = orderId;
+        this.restaurants = restaurants;
+    }
+
+    public Order() {
+    }
 }

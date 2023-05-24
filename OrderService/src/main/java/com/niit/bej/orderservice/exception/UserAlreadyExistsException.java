c225be1e88ff54547219b86f037aa3bd58a3b3b8
zebaamk <zebaamk@gmail.com>
2023-05-24 05:48:43 -0700

    UserAlreadyExistsException class is created

--- OrderService/src/main/java/com/niit/bej/orderservice/exception/UserAlreadyExistsException.java
+++ OrderService/src/main/java/com/niit/bej/orderservice/exception/UserAlreadyExistsException.java
@@ -5,4 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.CONFLICT, reason = "User already exists in database!")
 public class UserAlreadyExistsException extends Exception {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
 }

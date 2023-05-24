bbdb35c4e2bbbdd7dab7c353ebf7de483052ca3b
zebaamk <zebaamk@gmail.com>
2023-05-24 00:33:30 -0700

    Attributes are added to Dish class

--- OrderService/src/main/java/com/niit/bej/orderservice/domain/Dish.java
+++ OrderService/src/main/java/com/niit/bej/orderservice/domain/Dish.java
@@ -1,4 +1,10 @@
 package com.niit.bej.orderservice.domain;
 
 public class Dish {
+
+    private String name;
+    private int price;
+    private String category;
+    private String imageUrl;
+    private String description;
 }

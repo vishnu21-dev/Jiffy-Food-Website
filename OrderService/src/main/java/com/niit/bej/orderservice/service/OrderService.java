f63ec380b23d126500ff657085d06525c859d21f
zebaamk <zebaamk@gmail.com>
2023-05-24 23:18:02 -0700

    methods are added

--- OrderService/src/main/java/com/niit/bej/orderservice/service/OrderService.java
+++ OrderService/src/main/java/com/niit/bej/orderservice/service/OrderService.java
@@ -1,6 +1,8 @@
 package com.niit.bej.orderservice.service;
 
+import com.niit.bej.orderservice.domain.Cuisine;
 import com.niit.bej.orderservice.domain.Order;
+import com.niit.bej.orderservice.domain.Restaurant;
 import com.niit.bej.orderservice.domain.User;
 import com.niit.bej.orderservice.exception.*;
 
@@ -15,4 +17,8 @@ public interface OrderService {
     List<Order> getAllOrders(String userId) throws UserNotFoundException, OrderNotFoundException;
 
     User getRestaurantByUserLocation(User user, String restaurantName) throws UserNotFoundException, RestaurantNotFoundException;
+
+    Restaurant getRestaurantByPreference(Restaurant restaurant, String userId) throws UserNotFoundException, RestaurantNotFoundException;
+
+    List<Cuisine> getRestaurantByCuisine(Cuisine cuisine, String restaurantName) throws CuisineNotFoundException, RestaurantNotFoundException;
 }

03494b4167d4ac975dd2871506746e757e812fb6
zebaamk <zebaamk@gmail.com>
2023-05-24 22:49:45 -0700

    constructor is added

--- OrderService/src/main/java/com/niit/bej/orderservice/service/OrderService.java
+++ OrderService/src/main/java/com/niit/bej/orderservice/service/OrderService.java
@@ -3,8 +3,11 @@ package com.niit.bej.orderservice.service;
 import com.niit.bej.orderservice.domain.Order;
 import com.niit.bej.orderservice.domain.User;
 import com.niit.bej.orderservice.exception.OrderAlreadyExistsException;
+import com.niit.bej.orderservice.exception.OrderNotFoundException;
 import com.niit.bej.orderservice.exception.UserAlreadyExistsException;
 import com.niit.bej.orderservice.exception.UserNotFoundException;
+
+import java.util.List;
 
 public interface OrderService {
 
@@ -12,4 +15,5 @@ public interface OrderService {
 
     User addOrder(Order order, String userId) throws UserNotFoundException, OrderAlreadyExistsException;
 
+    List<Order> getAllOrders(String userId) throws UserNotFoundException, OrderNotFoundException;
 }

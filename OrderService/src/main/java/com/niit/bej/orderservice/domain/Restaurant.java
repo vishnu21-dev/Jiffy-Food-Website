4f16004ca8764608719ceb245ec6b3a9facf4dd2
zebaamk <zebaamk@gmail.com>
2023-05-24 22:28:11 -0700

    Changes made in Restaurant.java

--- OrderService/src/main/java/com/niit/bej/orderservice/domain/Restaurant.java
+++ OrderService/src/main/java/com/niit/bej/orderservice/domain/Restaurant.java
@@ -7,11 +7,13 @@ public class Restaurant {
 
     private String name;
     private List<Cuisine> cuisines;
+    private String preference;
     private Address address;
 
-    public Restaurant(String name, List<Cuisine> cuisines, Address address) {
+    public Restaurant(String name, List<Cuisine> cuisines, String preference, Address address) {
         this.name = name;
         this.cuisines = cuisines;
+        this.preference = preference;
         this.address = address;
     }
 
@@ -34,6 +36,14 @@ public class Restaurant {
         this.cuisines = cuisines;
     }
 
+    public String getPreference() {
+        return preference;
+    }
+
+    public void setPreference(String preference) {
+        this.preference = preference;
+    }
+
     public Address getAddress() {
         return address;
     }
@@ -47,12 +57,12 @@ public class Restaurant {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Restaurant that = (Restaurant) o;
-        return Objects.equals(name, that.name) && Objects.equals(cuisines, that.cuisines) && Objects.equals(address, that.address);
+        return Objects.equals(name, that.name) && Objects.equals(cuisines, that.cuisines) && Objects.equals(preference, that.preference) && Objects.equals(address, that.address);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, cuisines, address);
+        return Objects.hash(name, cuisines, preference, address);
     }
 
     @Override
@@ -60,6 +70,7 @@ public class Restaurant {
         return "Restaurant{" +
                 "name='" + name + '\'' +
                 ", cuisines=" + cuisines +
+                ", preference='" + preference + '\'' +
                 ", address=" + address +
                 '}';
     }

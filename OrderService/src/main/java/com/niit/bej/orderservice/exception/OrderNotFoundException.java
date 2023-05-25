2249aa15fe0fdfd03ba3052cf3f769af83497e96
zebaamk <zebaamk@gmail.com>
2023-05-24 22:56:22 -0700

    constructor is added

--- OrderService/src/main/java/com/niit/bej/orderservice/exception/OrderNotFoundException.java
+++ OrderService/src/main/java/com/niit/bej/orderservice/exception/OrderNotFoundException.java
@@ -5,4 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Order not found in database!")
 public class OrderNotFoundException extends Exception {
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
 }

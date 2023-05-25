91387edd5f5b47af3f9fc5e43f98efde7d7cfbe1
zebaamk <zebaamk@gmail.com>
2023-05-24 23:17:16 -0700

    constructor is created

--- OrderService/src/main/java/com/niit/bej/orderservice/exception/CuisineNotFoundException.java
+++ OrderService/src/main/java/com/niit/bej/orderservice/exception/CuisineNotFoundException.java
@@ -5,4 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Cuisine not found!")
 public class CuisineNotFoundException extends Exception {
+    public CuisineNotFoundException(String message) {
+        super(message);
+    }
 }

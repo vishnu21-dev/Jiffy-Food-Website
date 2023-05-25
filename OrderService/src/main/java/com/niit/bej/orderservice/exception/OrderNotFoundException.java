1fce497edfa133e0fc7e88bb5dac91d1b5ecacdb
zebaamk <zebaamk@gmail.com>
2023-05-24 22:47:12 -0700

    OrderNotFoundException class is created

---
+++ OrderService/src/main/java/com/niit/bej/orderservice/exception/OrderNotFoundException.java
@@ -0,0 +1,8 @@
+package com.niit.bej.orderservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Order not found in database!")
+public class OrderNotFoundException extends Exception {
+}

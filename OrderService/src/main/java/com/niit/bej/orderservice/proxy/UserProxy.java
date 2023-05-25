56120b3f32072a549fa77a347057ce8fe9d703f8
saikirangurijala123 <saikirangurijala123@gmail.com>
2023-05-25 11:16:35 +0530

    api-Gatway service is done

--- OrderService/src/main/java/com/niit/bej/orderservice/proxy/UserProxy.java
+++ OrderService/src/main/java/com/niit/bej/orderservice/proxy/UserProxy.java
@@ -2,6 +2,6 @@ package com.niit.bej.orderservice.proxy;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "authService", url = "localhost:8084")
+@FeignClient(name = "authService", url = "localhost:8083")
 public interface UserProxy {
 }

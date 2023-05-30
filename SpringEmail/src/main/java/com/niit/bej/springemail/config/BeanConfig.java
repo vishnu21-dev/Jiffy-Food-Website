7d87f214e605b34a73ec01b188b974b93c77467b
zebaamk <zebaamk@gmail.com>
2023-05-30 00:37:09 -0700

    jwtFilterBean method is added

--- SpringEmail/src/main/java/com/niit/bej/springemail/config/BeanConfig.java
+++ SpringEmail/src/main/java/com/niit/bej/springemail/config/BeanConfig.java
@@ -1,14 +1,17 @@
 package com.niit.bej.springemail.config;
 
+import com.niit.bej.springemail.filter.JWTFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class BeanConfig {
 
     @Bean
     public FilterRegistrationBean jwtFilterBean() {
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
-        filterRegistrationBean.setFilter(new JwtFilter());
+        filterRegistrationBean.setFilter(new JWTFilter());
         filterRegistrationBean.addUrlPatterns("/v7/*");
         return filterRegistrationBean;
     }

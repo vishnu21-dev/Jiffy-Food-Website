45c347339fe2baa956d6befcd79293e45402261a
zebaamk <zebaamk@gmail.com>
2023-05-30 21:20:04 -0700

    Email class is created

---
+++ SpringEmail/src/main/java/com/niit/bej/springemail/domain/Email.java
@@ -0,0 +1,10 @@
+package com.niit.bej.springemail.domain;
+
+public class Email {
+
+    private String toEmail;
+    private String body;
+    private String subject;
+    private String attachment;
+
+}

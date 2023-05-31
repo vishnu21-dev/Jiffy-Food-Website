bd577da805bfde59420d3c7a9bacc7931f096056
zebaamk <zebaamk@gmail.com>
2023-05-30 21:23:47 -0700

    Getters & Setters are added

--- SpringEmail/src/main/java/com/niit/bej/springemail/domain/Email.java
+++ SpringEmail/src/main/java/com/niit/bej/springemail/domain/Email.java
@@ -16,4 +16,36 @@ public class Email {
 
     public Email() {
     }
+
+    public String getToEmail() {
+        return toEmail;
+    }
+
+    public void setToEmail(String toEmail) {
+        this.toEmail = toEmail;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getAttachment() {
+        return attachment;
+    }
+
+    public void setAttachment(String attachment) {
+        this.attachment = attachment;
+    }
 }

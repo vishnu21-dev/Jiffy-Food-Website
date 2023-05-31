812a8148ae9501361471b094317bca37988ec391
zebaamk <zebaamk@gmail.com>
2023-05-30 23:02:16 -0700

    errors are modified

--- SpringEmail/src/main/java/com/niit/bej/springemail/SpringEmailApplication.java
+++ SpringEmail/src/main/java/com/niit/bej/springemail/SpringEmailApplication.java
@@ -1,7 +1,6 @@
 package com.niit.bej.springemail;
 
-import com.niit.bej.springemail.service.EmailSenderService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,22 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringEmailApplication {
 
-    @Autowired(required = true)
-    private EmailSenderService service;
-
     public static void main(String[] args) {
         SpringApplication.run(SpringEmailApplication.class, args);
     }
-
-
-//    @EventListener(ApplicationReadyEvent.class)
-//    public void triggerMail() throws MessagingException {
-//
-//        service.sendEmailWithAttachment("jiffyfoodApp@gmail.com",
-//                "Registration Confirmed! Thank you for registering with Jiffy. " +
-//                        "Check out our website for more details!",
-//                "Registration request confirmation",
-//                "D:\\WelcomeMessage.jpg");
-//    }
 
 }

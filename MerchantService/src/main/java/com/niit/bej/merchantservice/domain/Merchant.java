824118d0a69cff9a97cae4308e9f13fe1e950542
zebaamk <zebaamk@gmail.com>
2023-06-01 03:51:10 -0700

    Merchant class is modified

--- MerchantService/src/main/java/com/niit/bej/merchantservice/domain/Merchant.java
+++ MerchantService/src/main/java/com/niit/bej/merchantservice/domain/Merchant.java
@@ -3,6 +3,8 @@ package com.niit.bej.merchantservice.domain;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
+import java.util.Objects;
+
 @Document
 public class Merchant {
 
@@ -20,5 +22,60 @@ public class Merchant {
     }
 
     public Merchant() {
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Restaurant getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(Restaurant restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Merchant merchant = (Merchant) o;
+        return Objects.equals(emailId, merchant.emailId) && Objects.equals(password, merchant.password) && Objects.equals(location, merchant.location) && Objects.equals(restaurantName, merchant.restaurantName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(emailId, password, location, restaurantName);
+    }
+
+    @Override
+    public String toString() {
+        return "Merchant{" +
+                "emailId='" + emailId + '\'' +
+                ", password='" + password + '\'' +
+                ", location='" + location + '\'' +
+                ", restaurantName=" + restaurantName +
+                '}';
     }
 }

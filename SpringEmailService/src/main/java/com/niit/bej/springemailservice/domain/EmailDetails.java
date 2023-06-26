34e033b75b6122430b2fd7bb067e46a0e2650d04
zebaamk <zebaamk@gmail.com>
2023-06-26 10:41:46 -0700

    Domain class EmailDetails is created

---
+++ SpringEmailService/src/main/java/com/niit/bej/springemailservice/domain/EmailDetails.java
@@ -0,0 +1,19 @@
+package com.niit.bej.springemailservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class EmailDetails {
+
+    private String recipient;
+    private String msgBody;
+    private String subject;
+
+
+}

3ba3b8068d13da6e0b97ad3e75ac58b39ba7eb21
vishnuarumugam2112 <vishnuarumugam2112@gmail.com>
2023-06-26 12:46:44 +0530

    committed the changes

--- SpringEmail/src/main/java/com/niit/bej/springemail/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.niit.bej.springemail.service;
-
-import com.niit.bej.springemail.domain.Email;
-import jakarta.mail.MessagingException;
-
-public interface EmailService {
-
-    String sendSimpleEmail(Email email, String emailDetails) throws MessagingException;
-}

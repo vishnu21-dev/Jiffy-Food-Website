8f011f50e4ec72d5f094bf0fef6ac501c0937ecf
zebaamk <zebaamk@gmail.com>
2023-06-26 09:57:21 -0700

    SpringEmailApplication is deleted

--- SpringEmail/src/main/java/com/niit/bej/springemail/service/EmailService.java
+++
@@ -1,8 +0,0 @@
-package com.niit.bej.springemail.service;
-
-import com.niit.bej.springemail.domain.EmailDetails;
-
-public interface EmailService {
-
-    String sendSimpleMail(EmailDetails details);
-}

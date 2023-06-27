d885744f2993b4654c4967778ebc6a5aaa6cbfc5
zebaamk <zebaamk@gmail.com>
2023-06-26 21:09:32 -0700

    Domain class Favourite is modified

--- OrderService/src/main/java/com/niit/bej/orderservice/domain/Favourite.java
+++ OrderService/src/main/java/com/niit/bej/orderservice/domain/Favourite.java
@@ -5,15 +5,25 @@ import java.util.Objects;
 
 public class Favourite {
 
+    private int favouriteId;
     private List<Restaurant> restaurantList;
     private List<Dish> dishList;
 
-    public Favourite(List<Restaurant> restaurantList, List<Dish> dishList) {
+    public Favourite(int favouriteId, List<Restaurant> restaurantList, List<Dish> dishList) {
+        this.favouriteId = favouriteId;
         this.restaurantList = restaurantList;
         this.dishList = dishList;
     }
 
     public Favourite() {
+    }
+
+    public int getFavouriteId() {
+        return favouriteId;
+    }
+
+    public void setFavouriteId(int favouriteId) {
+        this.favouriteId = favouriteId;
     }
 
     public List<Restaurant> getRestaurantList() {
@@ -37,18 +47,19 @@ public class Favourite {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Favourite favourite = (Favourite) o;
-        return Objects.equals(restaurantList, favourite.restaurantList) && Objects.equals(dishList, favourite.dishList);
+        return favouriteId == favourite.favouriteId && Objects.equals(restaurantList, favourite.restaurantList) && Objects.equals(dishList, favourite.dishList);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(restaurantList, dishList);
+        return Objects.hash(favouriteId, restaurantList, dishList);
     }
 
     @Override
     public String toString() {
         return "Favourite{" +
-                "restaurantList=" + restaurantList +
+                "favouriteId=" + favouriteId +
+                ", restaurantList=" + restaurantList +
                 ", dishList=" + dishList +
                 '}';
     }
